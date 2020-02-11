acfd51b9874c6814481b38220ec0ed7bcc482d74
Martin Blasco <martin.blasco5@gmail.com>
2020-02-11 18:00:08 -0300

    Adding support for spotify in macos

--- zsh/lib/spotify.zsh
+++ zsh/lib/spotify.zsh
@@ -29,3 +29,14 @@ function get_current_song(){
 		org.mpris.MediaPlayer2.Player \
 		Metadata
 }
+
+if [ `uname -s` = "Darwin" ]; then
+	play_pause(){ osascript -e 'tell application "Spotify" to playpause' }
+	unset -f next_song
+	next_song(){ osascript -e 'tell application "Spotify" to next track'}
+	unset -f previous_song
+	previous_song(){
+		osascript -e ' tell application "Spotify" set player position to 0 previous track end tell' }
+	unset -f get_current_song
+	get_current_song(){ osascript -e 'tell application "Spotify" to name of current track as string' }
+fi

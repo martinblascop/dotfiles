77c0427f057284563bf78a1318efa57fb3f22bc7
Martin Blasco <martin.blasco5@gmail.com>
2020-08-11 09:31:13 -0300

    Fixing ls colors

--- zsh/lib/ls.zsh
+++ zsh/lib/ls.zsh
@@ -1,11 +1,11 @@
 if [[ "$OSTYPE" == "Linux" ]];then
 	alias ls='ls --color=tty'
+	[ -f ~/.dir_colors ] && eval "$(dircolors ~/.dir_colors)"
+	LS_COLORS="$LS_COLORS:ow=34;1;4"
+	zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 else
 	alias ls='ls -G'
+	export CLICOLOR=1
+	export LSCOLORS=BxGxdxdxCxegedabagacad
+	zstyle ':completion:*:default' list-colors ''
 fi
-
-[ -f ~/.dir_colors ] && eval "$(dircolors ~/.dir_colors)"
-
-LS_COLORS="$LS_COLORS:ow=34;1;4"
-
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}

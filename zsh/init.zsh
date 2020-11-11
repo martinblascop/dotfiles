16997deb21790e2efdf147019ef5f10e42d22c4e
Martin Blasco <martin.blasco5@gmail.com>
2020-11-11 13:14:35 -0300

    Added plugins for tmux

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -7,8 +7,3 @@ fpath=($ZSH/functions $ZSH/completions $fpath)
 for config_file ($ZSH/lib/*.zsh); do
 	source $config_file
 done
-
-[ -f $ZSH/plugins/zsh-autopair/zsh-autopair.plugin.zsh ] &&
-	source $ZSH/plugins/zsh-autopair/zsh-autopair.plugin.zsh
-[ -f $ZSH/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh ] &&
-	source $ZSH/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
---
+++ zsh/lib/repositories.zsh
@@ -0,0 +1,15 @@
+[ -d ~/.tmux/plugins ] || mkdir ~/.tmux/plugins
+[ -d ~/.tmux/plugins/tpm ] ||
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+[ -d $ZSH/plugins/zsh-autopair ] ||
+	git clone https://github.com/hlissner/zsh-autopair.git $ZSH/plugins/zsh-autopair
+
+[ -d $ZSH/plugins/zsh-autosuggestions ] ||
+	git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH/plugins/zsh-autosuggestions
+
+[ -f $ZSH/plugins/zsh-autopair/zsh-autopair.plugin.zsh ] &&
+	source $ZSH/plugins/zsh-autopair/zsh-autopair.plugin.zsh
+
+[ -f $ZSH/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh ] &&
+	source $ZSH/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh

d32a6f0652a1333e5f1d1b58e519df3fbea188e5
Martin Blasco <martinblascop@gmail.com>
2023-09-15 14:59:01 -0300

    Update alias for terraform

--- zsh/lib/alias.zsh
+++ zsh/lib/alias.zsh
@@ -6,5 +6,5 @@ alias rg='rg --colors match:fg:39 --colors path:fg:36 --colors line:fg:15'
 alias vimall='vim *'
 alias dco='docker-compose'
 alias dfimage="docker run -v /var/run/docker.sock:/var/run/docker.sock --rm alpine/dfimage" # usage dfimage -sV=1.36 <image>
+alias tf='terraform'
 which kubectl > /dev/null && alias k='kubectl'
-which terraform > /dev/null && alias tf='terraform'

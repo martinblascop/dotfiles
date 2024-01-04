e786e454db931584424f09fff4969d6b3e1b16f7
Martin Blasco <martinblascop@gmail.com>
2024-01-04 17:58:33 -0300

    Adding environment variables

--- zsh/lib/environment-variables.zsh
+++ zsh/lib/environment-variables.zsh
@@ -1,5 +1,7 @@
 # Docker
 export BUILDKIT_PROGRESS=plain
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+
 
 # aws
 export AWS_PAGER=""
@@ -10,3 +12,5 @@ export GH_PAGER=""
 # kubectl krew plugin
 export KREW_ROOT=~/.krew/bin
 export PATH=$PATH:$KREW_ROOT
+
+[ `uname -s` = "Darwin" ] && export PATH=$PATH:/opt/homebrew/bin

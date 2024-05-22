5a79639a95b5ad26db9d13d7963e5a8b8e3a2851
Martin Blasco <martinblascop@gmail.com>
2024-05-22 12:14:32 -0300

    Added dir for zsh completions

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -4,6 +4,14 @@ autoload -Uz compinit && compinit
 # add a function path
 fpath=($ZSH/functions $ZSH/completions $fpath)
 
+if [[ -z "$ZSH_CACHE_DIR" ]]; then
+    ZSH_CACHE_DIR="$ZSH/cache"
+fi
+
+# Create cache and completions dir and add to $fpath
+mkdir -p "$ZSH_CACHE_DIR/completions"
+(( ${fpath[(Ie)"$ZSH_CACHE_DIR/completions"]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
+
 for config_file ($ZSH/lib/*.zsh); do
 	source $config_file
 done

b7c85ca63e7a1e882a2eb8b79ca7e2c79763a835
Martin Blasco <martinblascop@gmail.com>
2025-02-12 19:44:06 -0300

    add tofu alias

--- zsh/lib/alias.zsh
+++ zsh/lib/alias.zsh
@@ -4,9 +4,9 @@ alias tmuxrc='vim ~/.tmux.conf'
 alias rg='rg --colors match:fg:39 --colors path:fg:36 --colors line:fg:15'
 alias vimall='vim *'
 alias dfimage="docker run -v /var/run/docker.sock:/var/run/docker.sock --rm alpine/dfimage" # usage dfimage -sV=1.36 <image>
-alias tf='terraform'
 which kubectl > /dev/null && abbrev-alias -g k='kubectl'
 which mysqlsh > /dev/null && abbrev-alias -g mysql='mysqlsh'
+which tofu > /dev/null && abbrev-alias -g tf='tofu'
 abbrev-alias -g dco=docker-compose
 abbrev-alias -g km=kustomize
 abbrev-alias -g mk=make

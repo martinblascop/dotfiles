94735ca592439053661c36c70e37141db7722c17
Martin Blasco <martinblascop@gmail.com>
2025-04-29 12:11:52 -0300

    update rg alias

--- zsh/lib/alias.zsh
+++ zsh/lib/alias.zsh
@@ -1,7 +1,7 @@
 alias vimrc='vim ~/.vimrc'
 alias zshrc='vim ~/.zshrc ~/.zshrc.local'
 alias tmuxrc='vim ~/.tmux.conf'
-alias rg='rg --colors match:fg:39 --colors path:fg:36 --colors line:fg:15'
+alias rg='rg --colors match:fg:39 --colors path:fg:36 --colors line:fg:15 --hidden --glob "!.git"'
 alias vimall='vim *'
 alias dfimage="docker run -v /var/run/docker.sock:/var/run/docker.sock --rm alpine/dfimage" # usage dfimage -sV=1.36 <image>
 alias diff='colordiff'

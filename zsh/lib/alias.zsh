96e494f49728ab3eb5e30e1fd702ff2603a19e8b
Martin Blasco <martinblascop@gmail.com>
2025-06-21 21:16:04 -0300

    update zsh/lib/alias.zsh

--- zsh/lib/alias.zsh
+++ zsh/lib/alias.zsh
@@ -5,7 +5,7 @@ alias rg='rg --colors match:fg:39 --colors path:fg:36 --colors line:fg:15 --hidd
 alias vimall='vim *'
 alias dfimage="docker run -v /var/run/docker.sock:/var/run/docker.sock --rm alpine/dfimage" # usage dfimage -sV=1.36 <image>
 alias diff='colordiff'
-which kubectl > /dev/null && abbrev-alias -g k='kubectl'
+which kubectl > /dev/null && abbrev-alias -g k="kubectl --context `kubectl ctx -c`"
 which mysqlsh > /dev/null && abbrev-alias -g mysql='mysqlsh'
 which tofu > /dev/null && abbrev-alias -g tf='tofu'
 abbrev-alias -g dco=docker-compose

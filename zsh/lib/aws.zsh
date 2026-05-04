aee30a9c834698c609a3539409922d8d51375496
Martin Blasco <martinblascop@gmail.com>
2026-05-04 15:31:54 -0300

    update zsh/lib/aws.zsh

--- zsh/lib/aws.zsh
+++ zsh/lib/aws.zsh
@@ -3,6 +3,8 @@ aws-get-volumes-by-autoscaling-group() {
     --filters "Name=tag:eks:nodegroup-name,Values=$1" "Name=instance-state-name,Values=running" \
     --query 'Reservations[*].Instances[*].BlockDeviceMappings[*].Ebs.VolumeId' \
     --output text | xargs -n1 printf "'%s'," | sed "s/,$// ; s/'//g")
-  echo $VOLS | sed "s/,/\n/g"
-  for vol in `echo $VOLS | sed "s/,/\ /g"` ; do echo "SELECT SUM(VolumeReadOps) FROM SCHEMA(\"AWS/EBS\", VolumeId) WHERE VolumeId = '$vol'" ; done
+  for vol in `echo $VOLS | sed "s/,/\ /g"` ; do
+    name=$(aws ec2 describe-volumes --volume-ids "$vol" --query "Volumes[0].Tags[?Key=='Name'].Value | [0]" --output text)
+    echo "$vol $name"
+  done
 }
